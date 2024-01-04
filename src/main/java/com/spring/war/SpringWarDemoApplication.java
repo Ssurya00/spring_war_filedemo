ea7a2b0bc80ca4f7b8ab6ad50555cfb789b821ef
Suryakant <94550070+Ssurya00@users.noreply.github.com>
2024-01-04 20:37:07 +0530

    first commit

---
+++ src/main/java/com/spring/war/SpringWarDemoApplication.java
@@ -0,0 +1,21 @@
+package com.spring.war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringWarDemoApplication {
+	
+	@GetMapping("/demowar")
+	public String greet() {
+		return "Hello War File";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWarDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/war/SpringWarDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.war;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWarDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
